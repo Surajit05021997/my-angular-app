793d425c34b705292a9e47220008f57de713c8af
Surajit Maity <surajitmaity12345@gmail.com>
2024-07-16 16:34:11 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from '../constant/dummy-users';
+import  { User } from '../interfaces/User';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  users = DUMMY_USERS;
+  selectedUser?: User;
+
+  onUserSelect(id: string) {
+    this.selectedUser = DUMMY_USERS.find((user) => user.id === id);
+  }
 }
